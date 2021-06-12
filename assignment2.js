8df5080c9e52bd80898ef6e7acfb4b04f333fa50
saimantmg01 <saimantmg01@gmail.com>
2021-06-12 16:26:43 -0400

    added filter, some, every, reduce implementation

--- assignment2.js
+++ assignment2.js
@@ -24,8 +24,8 @@ arr.forEach((x) => console.log(x));
 
 */
 
-//test more
-// MAP //
+//test more --need to test with original function.
+//MAP //
 // const array1 = [1, 4, 9, 16];
 // Array.prototype.myMap = function(callbackFn) {
 //     const new_array = [];
@@ -40,27 +40,92 @@ arr.forEach((x) => console.log(x));
 // const map1 = array1.myMap(x=>x*2);
 // console.log(map1);
 
+
+//FILTER //  --> works but gives empty items
+// Array.prototype.myFilter = function(callbackFn) {
+//     const new_array = [];
+//     for(let i = 0; i<this.length; i++) {
+//     if(callbackFn(this[i],i,this)) {
+//         new_array.push(this[i]);   //cnt use push function.
+//     }
+//     }
+//     return new_array;
+// };
+
+// const words = ['spray', 'limit', 'elite', 'exuberant', 'destruction', 'present'];
+
+// const result = words.myFilter(word => word.length > 6);
+
+// console.log(result);
+
+
+// SOME // --> test more
+// Array.prototype.mySome = function(callbackFn) {
+//     let bool = false;
+//     for(let i = 0; i< this.length; i++){
+//         if(callbackFn(this[i],i,this)) {
+//             bool = true;
+//             break;
+//         }
+//     }
+    
+//     return bool;
+// };
+
+// const array = [1, 2, 3, 4, 5];
+
+// // checks whether an element is even
+// const even = (element) => element % 2 === 0;
+
+// console.log(array.mySome(even));
+// expected output: true
+
+
+// EVERY // --> test properly more.
+// Array.prototype.myEvery = function(callbackFn) {
+//     let bool = true;
+//     for(let i =0; i< this.length; i++) {
+//         if(!callbackFn(this[i],i,this)) {
+//             bool = false;
+//             break;
+//         }
+//     }
+//     return bool;
+// };
+
+// const isBelowThreshold = (currentValue) => currentValue < 40;
+
+// const array1 = [1, 30, 39, 29, 10, 13];
+
+// console.log(array1.myEvery(isBelowThreshold));
+// // expected output: true
+
+
+// REDUCE // --> test more
+// Array.prototype.myReduce = function(callbackFn, initialValue) {
+//     let result = 0;
+//     if(initialValue) {
+//         result = initialValue;
+//     }
+//     for(let i = 0; i< this.length; i++) {
+//         result = callbackFn(result, this[i],i, this);
+//     }
+//     return result;
+// };
+
+// //test
+// const array1 = [1, 2, 3, 4];
+// const reducer = (accumulator, currentValue) => accumulator + currentValue;
+
+// // 1 + 2 + 3 + 4
+// console.log(array1.myReduce(reducer));
+// // expected output: 10
+
+// // 5 + 1 + 2 + 3 + 4
+// console.log(array1.myReduce(reducer, 5));
+// // expected output: 15
+
 /*
-// FILTER //
-Array.prototype.myFilter = function() {
-
-};
-
-// SOME //
-Array.prototype.mySome = function() {
-
-};
-
-// EVERY //
-Array.prototype.myEvery = function() {
-
-};
-
-// REDUCE //
-Array.prototype.myReduce = function() {
-
-};
-
 // INCLUDES //
 Array.prototype.myIncludes = function() {
 
